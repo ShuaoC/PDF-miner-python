6b6446a544b26ea6d4bfcf7b637094ea84fdc7f9
ShuaoC <felix19981228@gmail.com>
2022-03-30 14:33:37 -0400

    Testing Pypdf2 functions

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import PyPDF2
+
+file = open("sample.pdf","rb")
+menu = open("Menu.pdf","rb")
+
+reader = PyPDF2.PdfFileReader(file)
+menuReader = PyPDF2.PdfFileReader(menu)
+
+print(reader.numPages)
+
+print(menuReader.numPages)
