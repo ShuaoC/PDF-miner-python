9ca0edaca8669d96b38942f19d7f31502022a991
ShuaoC <felix19981228@gmail.com>
2022-03-30 16:51:12 -0400

    PDFminer implemented

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-import PyPDF2
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+import io
 
-file = open("sample.pdf","rb")
-menu = open("Menu.pdf","rb")
+def pdfminer(file, output): 
+    inFile = open(file, 'rb')
+    rM = PDFResourceManager()
+    data = io.StringIO()
+    TxtConverter = TextConverter(rM, data,laparams = LAParams())
+    interpreter = PDFPageInterpreter(rM, TxtConverter)
+    
+    for page in PDFPage.get_pages(inFile): 
+        interpreter.process_page(page)
+        
+    txt = data.getvalue()
+    with open(output, 'w') as f: 
+         f.write(txt)
 
-reader = PyPDF2.PdfFileReader(file)
-menuReader = PyPDF2.PdfFileReader(menu)
 
-print(reader.numPages)
-
-print(menuReader.numPages)
+   
+file = "test.pdf" 
+output = "info.txt"
+pdfminer(file, output)
